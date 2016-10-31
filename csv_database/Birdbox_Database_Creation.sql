ccb2ecf68e4c9d2ef20cb6d14ddd301b11cae567
Anthony Hauger <hauger.anthony@gmail.com>
2016-10-31 10:09:18 -0600

    Update Birdbox_Database_Creator.sql, still LOCAL
    Deletes Database if exists, deletes tables if exists; essentially starts from scratch each time it is run.  Doesn't read in first line of each CSV, so it just reads the table and doesn't create two table headers.

--- csv_database/Birdbox_Database_Creation.sql
+++ csv_database/Birdbox_Database_Creation.sql
@@ -1,22 +1,31 @@
+DROP DATABASE IF EXISTS Birdbox;
 CREATE DATABASE Birdbox;
 
 USE Birdbox;
 
-CREATE TABLE Display_Info (Bird_ID INTEGER, Text VARCHAR(45), Image VARCHAR(45), Colors VARCHAR(45));
+DROP TABLE IF EXISTS Display_Info;
+CREATE TABLE Display_Info (Bird_ID INTEGER, Common_Name VARCHAR(45), Text VARCHAR(45), Image VARCHAR(45), Color VARCHAR(45));
 
-CREATE TABLE Similarity (Bird_1_Text LONGTEXT, Bird_2_Text LONGTEXT, Bird_ID_1 INTEGER, Bird_ID_2 INTEGER, Sim_ID INTEGER);
+DROP TABLE IF EXISTS Similarity;
+CREATE TABLE Similarity (Sim_ID INTEGER, Bird_ID_1 INTEGER, Bird_ID_2 INTEGER, Bird_1_Text LONGTEXT, Bird_2_Text LONGTEXT);
 
-CREATE TABLE Filters (Common_Name VARCHAR(45), Sex VARCHAR(45), Family VARCHAR(45), Len FLOAT, Body_Mass FLOAT, Wingspan FLOAT, Notable_Features VARCHAR(45), C_Black VARCHAR(1), C_White VARCHAR(1), C_Brown VARCHAR(1), C_Red VARCHAR(1), C_Grey VARCHAR(1), C_Orange_Yellow VARCHAR(1), C_Green VARCHAR(1), C_Blue VARCHAR(1), C_Purple VARCHAR(1), Bird_ID INTEGER);
+DROP TABLE IF EXISTS Filters;
+CREATE TABLE Filters (Bird_ID INTEGER,Common_Name VARCHAR(45), Sex VARCHAR(45), Scientific_Name VARCHAR(45),Family VARCHAR(45), Length_cm FLOAT, Body_Mass_kg FLOAT, Wingspan_cm FLOAT, Notable_Features VARCHAR(45), C_Black VARCHAR(1), C_White VARCHAR(1), C_Brown VARCHAR(1), C_Red VARCHAR(1), C_Grey VARCHAR(1), C_Orange_Yellow VARCHAR(1), C_Green VARCHAR(1), C_Blue VARCHAR(1), C_Purple VARCHAR(1));
 
 LOAD DATA LOCAL INFILE '/home/user/CSCI-3308-Group-Project/csv_database/display_info.csv'
 INTO TABLE Display_Info
-FIELDS TERMINATED BY ',';
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES
+(Bird_ID,Common_Name,Text,Image,Color);
 
 LOAD DATA LOCAL INFILE '/home/user/CSCI-3308-Group-Project/csv_database/similarity.csv'
 INTO TABLE Similarity
-FIELDS TERMINATED BY ',';
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES 
+(Sim_ID, Bird_ID_1, Bird_ID_2, Bird_1_Text, Bird_2_text);
 
 LOAD DATA LOCAL INFILE '/home/user/CSCI-3308-Group-Project/csv_database/filter.csv'
 INTO TABLE Filters
-FIELDS TERMINATED BY ',';
-
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES 
+(Bird_ID, Common_Name, Sex, Scientific_Name, Family, Length_cm, Body_Mass_kg, Wingspan_cm, Notable_features, C_Black, C_White, C_Brown, C_Red, C_Grey, C_Orange_Yellow, C_Green, C_Blue, C_purple);
