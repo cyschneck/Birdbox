407c4e0eff59f7b4c5f83326a7e5342c4ef4ed45
Anthony Hauger <hauger.anthony@gmail.com>
2016-11-17 19:39:59 -0700

    Previous update with comments
    Added comments so everybody could understand what is going on :)

--- csv_database/Birdbox_Database_Creation.sql
+++ csv_database/Birdbox_Database_Creation.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS Birdbox;
+DROP DATABASE IF EXISTS Birdbox; #drops database, then creates it for sheer ease
 CREATE DATABASE Birdbox;
 
 USE Birdbox;
 
-DROP TABLE IF EXISTS Display_Info;
+DROP TABLE IF EXISTS Display_Info; #deletes previous table and then creates new table with these headers so no duplicates are possible on update
 CREATE TABLE Display_Info (Bird_ID INTEGER, Common_Name VARCHAR(45), Text VARCHAR(45), Image VARCHAR(45), Color VARCHAR(45));
 
 DROP TABLE IF EXISTS Similarity;
@@ -12,10 +12,10 @@ CREATE TABLE Similarity (Sim_ID INTEGER, Bird_ID_1 INTEGER, Bird_ID_2 INTEGER, B
 DROP TABLE IF EXISTS Filters;
 CREATE TABLE Filters (bird_id INTEGER, common_name VARCHAR(45), sex VARCHAR(45), scientific_name VARCHAR(45), family VARCHAR(45), lenth_lower FLOAT, length_upper FLOAT, body_mass_lower FLOAT, body_mass_upper FLOAT, wingspan_lower FLOAT, wingspan_upper FLOAT, NF_wing_bar VARCHAR(1), NF_long_neck VARCHAR(1), NF_colorful VARCHAR(1), NF_large_bill VARCHAR(1), NF_long_tail VARCHAR(1), NF_plumage VARCHAR(1), other VARCHAR(45) C_Black VARCHAR(1), C_White VARCHAR(1), C_Brown VARCHAR(1), C_Red VARCHAR(1), C_Grey VARCHAR(1), C_Orange_Yellow VARCHAR(1), C_Green VARCHAR(1), C_Blue VARCHAR(1), C_Purple VARCHAR(1));
 
-LOAD DATA LOCAL INFILE '/home/user/CSCI-3308-Group-Project/csv_database/display_info.csv'
-INTO TABLE Display_Info
-FIELDS TERMINATED BY ','
-IGNORE 1 LINES 
+LOAD DATA LOCAL INFILE '/home/user/CSCI-3308-Group-Project/csv_database/display_info.csv' #takes the file at this directory and puts it into the table below.  Since
+INTO TABLE display_info 																  #we're using .csv's, it parses by the delimeter ',' and ignores the first line so
+FIELDS TERMINATED BY ','																  #we only get the data and don't have to deal with the headers where the data should
+IGNORE 1 LINES 																			  #be.
 (Bird_ID,Common_Name,Text,Image,Color);
 
 LOAD DATA LOCAL INFILE '/home/user/CSCI-3308-Group-Project/csv_database/similarity.csv'
@@ -29,3 +29,4 @@ INTO TABLE Filters
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES 
 (bird_id, common_name, sex, scientific_name, family, length_lower, length_upper, body_mass_lower, body_mass_upper, wingspan_lower, wingspan_upper, NF_wing_bar, NF_long_neck, NF_colorful, NF_large_bill, NF_long_tail, NF_plumage, other, C_Black, C_White, C_Brown, C_Red, C_Grey, C_Orange_Yellow, C_Green, C_Blue, C_purple);
+
